e36e524fb2961a8e99a60f9c76a3e385dad810cf
Kyle Scully <scullykns@gmail.com>
2016-08-25 20:20:59 -0700

    more accordion

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -8,6 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccordionComponent implements OnInit {
 
+  accordionPanels = [
+    {
+      title: "Panel Title",
+      description: "This is the description that can be fed directly into the panel contents.",
+      list: ["Item 1","Item 2","Item 3", "Item 4"],
+      panelid: "ONE"
+    },
+    {
+      title: "Red Team",
+      description: "The red team is will help to establish and maintain traditions in efforts to prevent them from being forgotten.",
+      list: ["Abraham","Ulysses","Theodore", "Dwight"],
+      panelid: "TWO"
+    },
+    {
+      title: "Blue Team",
+      description: "The blue team will help to evaluate new advances and methods in efforts to improve the status quo for all.",
+      list: ["Franklin","Woodrow","Andrew", "Harry"],
+      panelid: "THREE"
+    }
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
